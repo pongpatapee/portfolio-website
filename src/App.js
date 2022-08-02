7074d2115d8156a9172ba7f76ade6908ba4ef9af
pongpatapee <pongpatapee1021@gmail.com>
2022-08-02 08:32:21 -0700

    added icons for dark/light mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import ThemeBtn from './components/ThemeBtn';
 
 function App() {
--- src/components/ThemeBtn.jsx
+++ src/components/ThemeBtn.jsx
@@ -1,12 +1,13 @@
 import useDarkMode from "../hooks/useDarkTheme";
+import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs'
 
 const ThemeBtn = () => {
     const [darkTheme, setDarkTheme] = useDarkMode();
     const handleMode = () => setDarkTheme(!darkTheme);
-    
+
     return (
         <button onClick={handleMode} className="cursor-pointer">
-            {darkTheme ? 'sun-icon' : 'moon-icon'}
+            {darkTheme ? <BsFillSunFill size={24} /> : <BsFillMoonFill size={22} />}
         </button>
     );
 }
