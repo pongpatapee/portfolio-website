653bb2f264b4f779c24dfa356ee552fab19ed346
pongpatapee <pongpatapee1021@gmail.com>
2022-08-06 10:13:24 -0700

    fixed resume viewer

--- src/components/ResumeViewer.jsx
+++ src/components/ResumeViewer.jsx
@@ -1,5 +1,6 @@
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import { useState } from "react";
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5';
+import PongpatapeeResume from '../assets/PongpatapeeResume.pdf';
 
 const ResumeViewer = () => {
   const [numPages, setNumPages] = useState(null);
@@ -12,14 +13,14 @@ const ResumeViewer = () => {
 
   return (
     <div className="resume-viewer flex flex-col justify-center items-center">
-      <a href="./PongpatapeeResume.pdf" download
+      <a href={PongpatapeeResume} download
           className="download-btn mb-4 text-xl">
         Download Resume
       </a>
-      <Document file="./PongpatapeeResume.pdf" 
+      <Document file={PongpatapeeResume}
                 onLoadSuccess={onDocumentLoadSuccess} 
                 onLoadError={console.error}>
-        <Page pageNumber={pageNumber} width="1000" className="border-2 border-black" />
+        <Page pageNumber={pageNumber} width={1000} className="border-2 border-black" />
       </Document>
       <p>
         Page {pageNumber} of {numPages}
