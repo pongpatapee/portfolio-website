76471c3bc2db04e20a76a97c545a4d90dfadc7fe
pongpatapee <pongpatapee1021@gmail.com>
2022-08-08 17:31:46 -0700

    added hover on project item

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,6 +1,6 @@
 const ProjectItem = ({ title, description, image, github, demo, tags }) => {
   return (
-    <div className="w-96 m-5 p-3 border-4 border-black">
+    <div className="w-96 m-5 p-3 border-4 border-black hover:scale-110 duration-200">
       <h1 className="text-2xl font-bold text-center">{title}</h1>
       <p>{description}</p>
       <img src={image} alt="demo-img" />
