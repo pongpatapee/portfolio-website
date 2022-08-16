525742f858defdbd0b48bf525478e78d0bac9ed6
pongpatapee <pongpatapee1021@gmail.com>
2022-08-16 12:14:54 -0700

    updated project list

--- src/assets/projectList.js
+++ src/assets/projectList.js
@@ -38,19 +38,60 @@ const projectList = [
   },
   {
     id: uuidv4(),
-    title: "ECE 461 NPM grader API",
-    description: "My team and I created an API to grade NPM modules for ECE 461 (Software Engineering)",
+    title: "Litter Detection AI",
+    description: `3rd place Hackathon project: Desgined a raspberry PI camera system 
+                  to aid in cleaning up the Purdue campus. The camera system module
+                  is meant to be installed on autonomous bots that are already
+                  roaming around campus. The camera system is designed to take pictures
+                  every few seconds and sends a request to Microsoft's Azure Object Detection
+                  to see if any litter has been detected. When litter is detected the raspberry PI
+                  would map its current location. The locations are visualized through a frontend 
+                  react app and shows a preview of the litter. 
+                  `,
+    image: images["ecomake-map.png"],
+    links: {
+      "github": "https://github.com/pongpatapee/Litter-Detection-A-EYE",
+    },
+    tags: [],
+  },
+  {
+    id: uuidv4(),
+    title: "ECE 461 NPM Module Trustworthiness Grader (Part 1)",
+    description: "str",
+    image: "img",
+    links: {
+      "github": "url",
+      "demo": "url"
+    },
+    tags: [],
+  },
+  {
+    id: uuidv4(),
+    title: "ECE 461 NPM Trustworthy Module Registry (Part 2)",
+    description: "",
     image: "img",
     links: {
       "github": "url",
       "demo": ""
     },
-    tags: ["Python", "REST API", "School", "Design"],
+    tags: [],
+  },
+  {
+    id: uuidv4(),
+    title: "NSTDA Internship",
+    description: "",
+    image: "img",
+    links: {
+      "github": "url",
+      "demo": ""
+    },
+    tags: [],
   },
   {
     id: uuidv4(),
     title: "Flappy Bird AI",
-    description: "Created a flappy bird AI using the NEAT genetic algorithm with TensorFlowjs",
+    description: `Created a flappy bird AI by implementing the NEAT genetic algorithm 
+                  with TensorFlowjs`,
     image: "img",
     links: {
       "github": "https://github.com/pongpatapee/Flappy-Bird-AI",
@@ -61,11 +102,48 @@ const projectList = [
   {
     id: uuidv4(),
     title: "Self-Driving Car",
-    description: "Created a self-driving car AI & Neural Network visualizer in vanilla JS using a simple genetic algorithm",
+    description: `Created a self-driving car AI & Neural Network visualizer in vanilla JS 
+                  using a simple genetic algorithm`,
     image: images['self-driving-car.png'],
     links: {
       "github": "https://github.com/pongpatapee/self-driving-car-js",
       "demo": "https://pongpatapee.github.io/self-driving-car-js/"
+    },
+    tags: [],
+  },
+  {
+    id: uuidv4(),
+    title: "something name WebDev Bootcamp",
+    description: "",
+    image: "img",
+    links: {
+      "github": "url",
+      "demo": ""
+    },
+    tags: [],
+  },
+  {
+    id: uuidv4(),
+    title: "Undergraduate ML research",
+    description: "",
+    image: "img",
+    links: {
+      "github": "url",
+      "demo": ""
+    },
+    tags: [],
+  },
+  {
+    id: uuidv4(),
+    title: "Raspberry PI Remote Light Switch",
+    description: `Deployed a Flask webserver on the raspberry PI to listen to requests
+                  to control servo motors to switch the lights on/off. Interact with 
+                  Light switch through the Flask website on any device (phones, tablets, computers)
+                  `,
+    image: "img",
+    links: {
+      "github": "url",
+      "demo": ""
     },
     tags: [],
   },
---
+++ src/assets/projects.md
@@ -0,0 +1,19 @@
+project list:
+
+- litter detection ai
+- ece 461 part 1
+- ece 461 part 2
+- nstda internship stuff
+- web dev bootcamp stuff
+- ocr app (kivy + android studio)
+- self driving car
+- flappy bird ai
+- VIP tensorflow stuff (mnist, music generation, designing neuralnet/resnet)
+- r place clone?
+- odin project stuff
+- react password generator
+- c++ zork?
+- other p5 projects?
+- FARM stack todo
+- Discord bot
+- raspberry light switch
