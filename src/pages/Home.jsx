1d5d982c9364b2b171717969144d3be95e46d8f3
pongpatapee <pongpatapee1021@gmail.com>
2022-08-26 02:01:09 -0400

    updated homepage to include my name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,12 @@ const Home = () => {
         className="flex flex-col justify-center items-center h-screen
                   bg-white light-dark-theme"
       >
-        <h1 className="text-6xl mb-10 font-bold font-bubblyPixel">
-          Welcome to my Portfolio!
+        <h1 className="text-5xl font-bold font-bubblyPixel mb-3">
+          Dan Peerapatanapokin
         </h1>
+        <h2 className="text-4xl font-bubblyPixel mb-5 ">
+          Welcome to my Portfolio!
+        </h2>
         <Link to="/about">
           <button className="home-btn">About Me</button>
         </Link>
