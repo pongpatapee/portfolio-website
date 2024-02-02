dc8c39759aa2319f2d455c8ae337a4e66b55497d
pongpatapee <pongpatapee1021@gmail.com>
2024-02-02 18:53:21 -0500

    feat: remove boilerplate and initialize home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="text-6xl font-bold">Hi</div>
+    </div>
+  );
+}
